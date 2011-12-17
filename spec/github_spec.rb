60dae4e781aeb27cd0b1e905a0d705788dac10cd
Tariqul Islam <tareq@webkutir.net>
2011-12-17 14:22:49 +0600

    some changes...

--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -8,29 +8,29 @@ describe GitHub do
       GitHub.issue("fixes #34").should be_nil
     end
 
-    it "should return issue number prefixed by issue#" do
-      GitHub.issue("fixes issue#34").should == "34"
+    it "should return issue number prefixed by issue-" do
+      GitHub.issue("fixes issue-34").should == "34"
     end
 
-    it "should return issue number prefixed by ISSUE#" do
-      GitHub.issue("fixes ISSUE#34").should == "34"
+    it "should return issue number prefixed by ISSUE-" do
+      GitHub.issue("fixes ISSUE-34").should == "34"
     end
   end
 
   describe "closed_issue" do
     it "should return issue number of closed issue" do
-      GitHub.closed_issues("fixes issue#34").should == ["34"]
-      GitHub.closed_issues("fixes ISSUE#34").should == ["34"]
+      GitHub.closed_issues("fixes issue-34").should == ["34"]
+      GitHub.closed_issues("fixes ISSUE-34").should == ["34"]
       GitHub.closed_issues("fixes #34").should == ["34"]
     end
 
     it "should return nil when issue not closed" do
-      GitHub.closed_issues("references ISSUE#34").should == []
+      GitHub.closed_issues("references ISSUE-34").should == []
     end
 
     it "should yield each issue to a given block" do
       found_issues = []
-      GitHub.closed_issues "fixes issue#34 and closes issue#35" do |issue|
+      GitHub.closed_issues "fixes issue-34 and closes issue-35" do |issue|
         found_issues.push issue
       end
       found_issues.should == ["34", "35"]
@@ -38,13 +38,13 @@ describe GitHub do
   end
 
   describe "nonclosing_issue" do
-    it "should return issue number of issue prefixed by issue#" do
-      GitHub.nonclosing_issues("changes issue#34").should == ["34"]
-      GitHub.nonclosing_issues("changes ISSUE#34").should == ["34"]
+    it "should return issue number of issue prefixed by issue-" do
+      GitHub.nonclosing_issues("changes issue-34").should == ["34"]
+      GitHub.nonclosing_issues("changes ISSUE-34").should == ["34"]
     end
 
     it "should return nil when issue doesnt have prefix" do
-      GitHub.nonclosing_issues("changes #34").should == []
+      GitHub.nonclosing_issues("changes -34").should == []
     end
 
     it "should return nil when no issue referenced" do
@@ -52,16 +52,16 @@ describe GitHub do
     end
 
     it "should return nil when issue referenced is being closed" do
-      GitHub.nonclosing_issues("fixes issue#34").should == [] 
-      GitHub.nonclosing_issues("fixes ISSUE#34").should == [] 
+      GitHub.nonclosing_issues("fixes issue-34").should == [] 
+      GitHub.nonclosing_issues("fixes ISSUE-34").should == [] 
       GitHub.nonclosing_issues("fixes #34").should  == []
       GitHub.nonclosing_issues("closes #34").should == []
-      GitHub.nonclosing_issues("Closes issue#34").should == [] 
+      GitHub.nonclosing_issues("Closes issue-34").should == [] 
     end
 
     it "should yield each issue to a given block" do
       found_issues = []
-      GitHub.nonclosing_issues "changes issue#22 and fixes issue#34 and updates ISSUE#35" do |issue|
+      GitHub.nonclosing_issues "changes issue-22 and fixes issue#34 and updates ISSUE-35" do |issue|
         found_issues.push issue
       end
       found_issues.should == ["22", "35"]
